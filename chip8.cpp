19d4b630ab6849dae37a9a930b3f96558be931b6
Aviraj Newatia <projectavi.exe@gmail.com>
2021-12-03 14:44:29 +0530

    Added more opcodes
    Implemented the functions for 5/6 more opcodes

--- chip8.cpp
+++ chip8.cpp
@@ -249,8 +249,123 @@ void Chip8::OP_Dxyn() {
             }
         }
     }
+}
 
+void Chip8::OP_Ex9E() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
 
+    uint8_t key = registers[Vx];
+
+    if (keypad[key]) {
+        programCount += 2;
+    }
+}
+
+void Chip8::OP_ExA1() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    uint8_t key = registers[Vx];
+
+    if (!keypad[key]) {
+        programCount += 2;
+    }
+}
+
+void Chip8::OP_Fx07() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    registers[Vx] = delayTimer;
+}
+
+void Chip8::OP_Fx0A() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    if (keypad[0])
+    {
+        registers[Vx] = 0;
+    }
+    else if (keypad[1])
+    {
+        registers[Vx] = 1;
+    }
+    else if (keypad[2])
+    {
+        registers[Vx] = 2;
+    }
+    else if (keypad[3])
+    {
+        registers[Vx] = 3;
+    }
+    else if (keypad[4])
+    {
+        registers[Vx] = 4;
+    }
+    else if (keypad[5])
+    {
+        registers[Vx] = 5;
+    }
+    else if (keypad[6])
+    {
+        registers[Vx] = 6;
+    }
+    else if (keypad[7])
+    {
+        registers[Vx] = 7;
+    }
+    else if (keypad[8])
+    {
+        registers[Vx] = 8;
+    }
+    else if (keypad[9])
+    {
+        registers[Vx] = 9;
+    }
+    else if (keypad[10])
+    {
+        registers[Vx] = 10;
+    }
+    else if (keypad[11])
+    {
+        registers[Vx] = 11;
+    }
+    else if (keypad[12])
+    {
+        registers[Vx] = 12;
+    }
+    else if (keypad[13])
+    {
+        registers[Vx] = 13;
+    }
+    else if (keypad[14])
+    {
+        registers[Vx] = 14;
+    }
+    else if (keypad[15])
+    {
+        registers[Vx] = 15;
+    }
+    else
+    {
+        programCount -= 2;
+    }
+}
+
+void Chip8::OP_Fx15() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    delayTimer = registers[Vx];
+}
+
+void Chip8::OP_Fx18() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    soundTimer = registers[Vx];
+}
+
+void Chip8::OP_Fx1E() {
+    uint8_t Vx = (opcode & 0x00F00u) >> 8u;
+
+    indexReg += registers[Vx];
 }
 
 Chip8::Chip8() {
